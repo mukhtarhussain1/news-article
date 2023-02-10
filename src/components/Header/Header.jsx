075f684dfc2e852d5cd4926f7bc2040fc2075425
Mukhtar Hussain <mukhtar.hussai9@gmail.com>
2023-02-10 18:09:54 +0500

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const nav = document.querySelector(".main-header");
 window.addEventListener("scroll", fixNav);
 
 function fixNav() {
-  if (window.scrollY > nav.offsetHeight + 250) {
+  if (window.scrollY > nav.offsetHeight + 150) {
     nav.classList.add("active");
   } else {
     nav.classList.remove("active");
